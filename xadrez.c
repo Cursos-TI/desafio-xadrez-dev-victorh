b7f034f26d4b0dc0f0df0d054eb5eea7a8e3c8df
dev-victorh <dev_victor@outlook.com>
2025-03-23 01:29:40 +0000

    Atualização para o nível mestre.

--- xadrez.c
+++ xadrez.c
@@ -1,40 +1,53 @@
 #include <stdio.h>
 
-int main() {
-    int j=1; //contador de fluxo para o loop do bispo, rainha e cavalo
+void movtorre(int n){
+        if (n>0){
+                movtorre(n-1);//recursividade sendo aplicada antes para que o movimento seja impresso na ordem correta
+                printf("%d. Direita\n", n); //para facilitar visualização, o contador de fluxo n é usado para numerar os movimentos
+        }
+}
 
+void movbispo(int n){
+        if (n>0){
+                printf(" Direita\n");
+                movbispo(n-1);
+        }
+}
+
+void movrainha(int n){
+        if (n>0){
+                movrainha(n-1);//recursividade sendo aplicada antes para que o movimento seja impresso na ordem correta
+                printf("%d. Esquerda\n", n); //para facilitar visualização, o contador de fluxo n é usado para numerar os movimentos
+        }
+}
+
+int main() {
     printf("Movimento da Torre:\n"); //anunciando o movimento da torre
-    for (int i=1; i<=5; i++){ //controle de loop usando 'for' para garantir que o movimento seja feito 5 vezes
-            printf("%d. Direita\n", i); //para facilitar visualização, o contador de fluxo é usado para numerar os movimentos
-    }
+    movtorre(5); //chamando a função movtorre com o valor 5 para o movimento da torre
 
     printf("\n");
 
     printf("Movimento do Bispo:\n"); //anunciando o movimento do bispo
-    while (j<=5){ //controle de loop usando 'while' para garantir que o movimento seja feito 5 vezes
-            printf("%d. Cima, Direita\n", j); //para facilitar visualização, o contador de fluxo é usado para numerar os movimentos
-            j++;
+    for (int i = 1; i <= 5; i++)
+    {
+        printf("%d. Cima, ", i);//imprimindo movimento vertical no loop externo.
+        movbispo(1); //movimento horizontal no loop interno. Isto pode ser considerado aninhado pois existe a recursividade dentro do for.
     }
-
-    j=1; //resetando o contador de fluxo
+    
     printf("\n");
 
     printf("Movimento da Rainha:\n"); //anunciando o movimento da rainha
-    do { //controle de loop usando 'do while' para garantir que o movimento seja feito 8 vezes
-            printf("%d. Esquerda\n", j); //para facilitar visualização, o contador de fluxo é usado para numerar os movimentos
-            j++;
-    } while (j<=8);
+    movrainha(8); //chamando a função movrainha com o valor 8 para o movimento da rainha
 
-    j=1; //resetando o contador de fluxo
     printf("\n");
     
-    printf("Movimento do Cavalo:\n");//anunciando o movimento do cavalo
-    for (int i=1; i<=1; i++){
+    printf("Movimento do Cavalo:\n");//anunciando o movimento do cavalo                             
+    for (int i=1, j=1; i<=1 && j<=2; i++){ // utilizando mais de uma variável e condições
         while (j<=2){
-                printf("%d. Baixo\n", j); //para facilitar visualização, o contador de fluxo é usado para numerar os movimentos
+                printf("%d. Cima\n", j); //para facilitar visualização, o contador de fluxo é usado para numerar os movimentos
                 j++;
         }
-        printf("%d. Esquerda\n", j); //para facilitar visualização, o contador de fluxo é usado para numerar os movimentos
+        printf("%d. Direita\n", j); //para facilitar visualização, o contador de fluxo é usado para numerar os movimentos
     }
 
     return 0;
